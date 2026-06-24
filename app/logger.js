2e8d81285d73c38fb5ed97ee2c4a93d6750161cf
Gabriel Mello <gabriel.mello@alunos.ifsuldeminas.edu.br>
2026-06-24 08:24:44 -0300

    Acerto no Logger.js para adequar a permissao ao diretorio

--- app/logger.js
+++ app/logger.js
@@ -1,5 +1,17 @@
 const winston = require("winston");
 
+// 1. Criamos a lista de transports começando apenas com o Console
+const defaultTransports = [
+  new winston.transports.Console()
+];
+
+// 2. Se NÃO for o ambiente de testes do GitHub Actions/Jest, adicionamos o arquivo
+if (process.env.NODE_ENV !== 'test') {
+  defaultTransports.push(
+    new winston.transports.File({ filename: '/var/log/app/app.log' })
+  );
+}
+
 const logger = winston.createLogger({
   level: "info",
 
@@ -8,10 +20,8 @@ const logger = winston.createLogger({
     winston.format.json()
   ),
 
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: '/var/log/app/app.log' })
-  ]
+  // 3. Passamos a nossa lista dinâmica aqui para o Winston
+  transports: defaultTransports
 });
 
 module.exports = logger;
